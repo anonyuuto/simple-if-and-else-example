35a94e79110fb9175f33ef33722d011b4a11bbea
Your Name <yuut@tuta.io>
2023-09-01 16:49:36 +0200

    code

---
+++ main.js
@@ -0,0 +1,20 @@
+document.write(String(watt));
+
+document.write('&nbsp YEN!!!&nbsp&nbsp');
+
+// this thing is new
+
+if (watt>1200) {
+
+  document.write('you rich af');
+
+
+
+}
+else {
+
+  document.write('get some bitches!');
+
+
+
+}
